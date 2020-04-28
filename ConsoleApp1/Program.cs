84475883dea20c8d625b044fc8dbb89637f7d6a9
sean wilson <sean0699@gmail.com>
2020-04-28 17:36:01 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+class Program
+{
+    public static void summation(int num)
+    {
+        int i;
+        int sum = 0;
+        for (i = 0; i < num + 1; i++)
+        {
+            sum = sum + i;
+        }
+        Console.WriteLine(sum);
+    }
+    static void Main(string[] args)
+    {
+        summation(4);
+    }
+}
+
